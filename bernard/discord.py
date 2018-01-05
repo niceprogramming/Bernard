ef4d1284284aeeeb9d0e404a19c6b97b1ae2edbd
ILiedAboutCake <iliedaboutthecake@gmail.com>
2018-01-04 21:46:46 -0500

    add game status setting on bot launch

--- bernard/discord.py
+++ bernard/discord.py
@@ -2,6 +2,7 @@ print("%s loading..." % __name__)
 
 from . import config
 
+import asyncio
 import discord
 from discord.ext import commands
 
@@ -9,7 +10,11 @@ bot = commands.Bot(command_prefix='!', description='Bernard, for Discord. Made w
 
 @bot.event
 async def on_ready():
-    print('{0} Logged in as "{0.user.name} ID:{0.user.id}"'.format(__name__, bot))
+    print('{0} Logged in as "{1.user.name} ID:{1.user.id}"'.format(__name__, bot))
+    await asyncio.sleep(5)
+    
+    print('{0} Setting game status as in as "{1}"'.format(__name__, config.cfg['bernard']['gamestatus']))
+    await bot.change_presence(game=discord.Game(name=config.cfg['bernard']['gamestatus']))
 
 def objectFactory(snowflake):
 	return discord.Object(snowflake)
