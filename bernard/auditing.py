2966104948811aa3d2ae849df78625b1a5514709
ILiedAboutCake <iliedaboutthecake@gmail.com>
2018-01-08 20:45:13 -0500

    should probably include the example config files

--- bernard/auditing.py
+++ bernard/auditing.py
@@ -40,12 +40,12 @@ async def discord_invites(msg):
         return
 
     #if the config is using the @everyone role
-    if config.cfg['auditing']['invites']['highest_role_blocked'] == "everyone":
+    if config.cfg['auditing']['invites']['lowest_role_blocked'] == "everyone":
         if msg.author.top_role.is_everyone == True:
             await discord.bot.delete_message(msg)
             await discord.bot.send_message(msg.channel, "⚠️ {0} Members without a role are unable to post Discord invites.".format(msg.author.mention))
             print("{0}: INFO deleted invite under reason: 'everyone role'".format(__name__))
-    elif msg.author.top_role.id == config.cfg['auditing']['invites']['highest_role_blocked']:
+    elif msg.author.top_role.id == config.cfg['auditing']['invites']['lowest_role_blocked']:
             await discord.bot.delete_message(msg)
             await discord.bot.send_message(msg.channel, "⚠️ {0} Your role does not meet the minimum requirements to post Discord invites.".format(msg.author.mention))
             print("{0}: INFO deleted invite under reason: 'underpowered role'".format(__name__))
