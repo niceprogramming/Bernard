51f52737ffacbe86c1f402681c4cb823af3fb7b2
ILiedAboutCake <iliedaboutthecake@gmail.com>
2018-01-23 21:58:06 -0500

    add in on_message_delete() actions and mod logging

--- bernard/deleted.py
+++ bernard/deleted.py
@@ -15,4 +15,13 @@ async def on_message_delete(message):
     if common.isDiscordMainServer(message.server.id) is not True:
         return
 
-    pass
+    if message.attachments and message.content == "":
+        msg = "{0.author.mention} (Name:`{0.author}` ID:`{0.author.id}`) in {0.channel.mention} \n Deleted Attachment: `{0.attachments[0][filename]}` \n URL: <{0.attachments[0][url]}>".format(message)
+    elif message.attachments and message.content != "":
+        msg = "{0.author.mention} (Name:`{0.author}` ID:`{0.author.id}`) in {0.channel.mention} \n Message: \"`{0.content}`\" \n\n Deleted Attachment: `{0.attachments[0][filename]}` \n URL: <{0.attachments[0][url]}>".format(message)
+    else:
+        msg = "{0.author.mention} (Name:`{0.author}` ID:`{0.author.id}`) in {0.channel.mention} \n Message: \"`{0.content}`\" \n\n".format(message)
+
+    await discord.bot.send_message(discord.mod_channel(), "{0} **Caught Deleted Message!** {1}".format(common.bernardUTCTimeNow(), msg))
+
+    analytics.onMessageProcessTime(msgProcessStart, analytics.getEventTime())
