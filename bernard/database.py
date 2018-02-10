9c296a3977177e9dea2fd8785bb65b8f64ea13dd
ILiedAboutCake <iliedaboutthecake@gmail.com>
2018-02-10 01:09:33 -0500

    add db lock check on both read and write actions and fail to start bot if db locked

--- bernard/database.py
+++ bernard/database.py
@@ -2,10 +2,26 @@ from . import config
 
 import sqlite3
 import logging
+import time
 
 logger = logging.getLogger(__name__)
 logger.info("loading...")
 
 #sqlite
-dbConn = sqlite3.connect(config.cfg['bernard']['database']['sqlite'], check_same_thread=False)
+dbConn = sqlite3.connect(config.cfg['bernard']['database']['sqlite'], check_same_thread=False, timeout=config.cfg['bernard']['database']['timeout'])
 dbCursor = dbConn.cursor()
+
+#check if the db is locked 
+def check_db_lock():
+    try:
+        dbCursor.execute("SELECT * FROM journal_jobs")
+        result = dbCursor.fetchall()
+
+        dbCursor.execute('INSERT INTO journal_jobs(module, job, time, runtime, result) VALUES(?,?,?,?,?)', (__name__, "sql_health", time.time(), 0, "OK"))
+        dbConn.commit()
+
+    except sqlite3.OperationalError:
+        logger.critical("Unable to start bot. Unrecoverable error in database locked.")
+        exit()
+
+check_db_lock()
